dc1afd2e0bf1d8d15156402291b503f20f7ca4c6
Naveen <naveen@nbmacmini07.local>
2018-04-25 21:37:58 +0530

    Update

--- src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
+++ src/main/java/com/marianhello/bgloc/BackgroundGeolocationFacade.java
@@ -72,19 +72,7 @@ public class BackgroundGeolocationFacade {
     private org.slf4j.Logger logger;
 
     Messenger mMessenger;
-public BackgroundGeolocationFacade(Context context){
-            mContext = context;
 
-        UncaughtExceptionLogger.register(context);
-
-        logger = LoggerManager.getLogger(BackgroundGeolocationFacade.class);
-        LoggerManager.enableDBLogging();
-
-        logger.info("Initializing plugin");
-
-        mMainhandler = new Handler(context.getMainLooper());
-        NotificationHelper.registerAllChannels(getContext());
-}
     public BackgroundGeolocationFacade(Context context, PluginDelegate delegate) {
         mContext = context;
         mDelegate = delegate;
